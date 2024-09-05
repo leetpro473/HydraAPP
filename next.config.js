8351c1217728abf68e53b6348dddfd26b0df46ef
luiskkkkkkkkkkkkkk <80022664+leetpro473@users.noreply.github.com>
2024-09-05 10:59:31 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true,
-	},
-	images: {
-		domains: ["img.clerk.com", "utfs.io", "res.cloudinary.com"],
-	},
 };
 
 module.exports = nextConfig;
